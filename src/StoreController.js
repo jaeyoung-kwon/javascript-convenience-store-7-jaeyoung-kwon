2c71f284c3a81eceec3d6c6f40fd184e7f80e2ac
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-11-07 14:07:30 +0900

    feat: 입력 값을 형태를 검증

--- src/StoreController.js
+++ src/StoreController.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import ConvenienceStore from './ConvenienceStore.js';
+import { throwWoowaError } from './lib/util.js';
 import Input from './View/Input.js';
 import Output from './View/Output.js';
 
@@ -11,14 +12,56 @@ class StoreController {
   async init() {
     this.#printInit();
 
-    const input = await Input.getPurchaseProducts();
+    const purchaseProducts = await this.#getValidatedPurchaseProducts();
 
-    Console.print(input);
+    Console.print(purchaseProducts);
   }
 
   #printInit() {
     Output.printWelcomeMessage();
     Output.printInventory(this.convenienceStore.inventory);
+  }
+
+  async #getValidatedPurchaseProducts() {
+    const purchaseProductsInput = await Input.getPurchaseProducts();
+
+    const parsedPurchaseProducts = this.#parsePurchaseProducts(purchaseProductsInput);
+    this.#validatePurchaseProducts(parsedPurchaseProducts);
+
+    return parsedPurchaseProducts;
+  }
+
+  #parsePurchaseProducts(purchaseProductsInput) {
+    const purchaseProducts = purchaseProductsInput
+      .split(',')
+      .map((product) => {
+        this.#validateProductInputForm(product);
+        return product.slice(1, -1);
+      })
+      .map((product) => product.split('-'))
+      .map(([name, quantity]) => ({ name: name.trim(), quantity: Number(quantity) }));
+
+    return purchaseProducts;
+  }
+
+  #validatePurchaseProducts(purchaseProducts) {
+    const { inventory } = this.convenienceStore;
+
+    purchaseProducts.forEach(({ name, quantity }) => {
+      const inventoryProduct = inventory[name];
+
+      if (!name || !quantity) throwWoowaError('올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.');
+
+      if (!inventoryProduct) throwWoowaError('존재하지 않는 상품입니다. 다시 입력해 주세요.');
+
+      if (inventoryProduct.regularStock + inventoryProduct.promotionStock < quantity)
+        throwWoowaError('재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.');
+    });
+  }
+
+  #validateProductInputForm(productString) {
+    if (!productString.startsWith('[')) throwWoowaError('올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.');
+    if (!productString.endsWith(']')) throwWoowaError('올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.');
   }
 }
 
