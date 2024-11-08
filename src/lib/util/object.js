17a87c5d8da12bbf095dfb74c841c5e661b11672
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-11-08 23:41:16 +0900

    refactor: object 유틸 함수 10라인으로 제한

--- src/lib/util/object.js
+++ src/lib/util/object.js
@@ -2,12 +2,9 @@ export const copyObject = (obj) => {
   if (typeof obj !== 'object' || obj === null) {
     return obj;
   }
-
   const deepCopyObj = {};
-
   Object.entries(obj).forEach(([key, value]) => {
     deepCopyObj[key] = copyObject(value);
   });
-
   return deepCopyObj;
 };
